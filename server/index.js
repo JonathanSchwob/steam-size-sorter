aef04e76f17bb244928d79f37122c6ee378f7130
JonathanSchwob <jonschwob@gmail.com>
2025-07-03 19:22:06 -0700

    add Steam Strategy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import 'dotenv/config'
+
+const express = require('express');
+const passport = require('passport');
+const SteamStrategy = require('passport-steam').Strategy;
+const app = express();
+const port = 3000;
+const config = {
+  steamAPIKey: process.env.STEAM_API_KEY
+};
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +18,17 @@ app.get('/ping', (req, res) => {
   res.send('pong!')
 })
 
+passport.use(new SteamStrategy({
+  returnURL: 'http://localhost:3000/auth/steam/return',
+  realm: 'http://localhost:3000/',
+  apiKey: config.steamAPIKey,
+},
+function(identifier, profile, done) {
+  User.findByOpenID({ openId: identifier }, function (err, user) {
+    return done(err, user);
+  });
+}
+));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
